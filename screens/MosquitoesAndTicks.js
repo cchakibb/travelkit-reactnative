6bf4696671cf99bd1af034ddc571d8fad29006f7
guigui-bubu <58784086+guigui-bubu@users.noreply.github.com>
2020-03-06 13:48:33 +0100

    ajout de component btn

--- screens/MosquitoesAndTicks.js
+++ screens/MosquitoesAndTicks.js
@@ -2,34 +2,23 @@ import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 import HeaderTopImage from "../components/HeaderTopImage";
+import BtnIcone from "../components/BtnIcone";
 
 export default function MosquitoesAndTicks() {
   const navigation = useNavigation();
   return (
     <View>
       <HeaderTopImage title={"Moustiques et tiques"}></HeaderTopImage>
-      <View>
-        <Text style={{ fontSize: 18, textAlign: "center", marginTop: 10 }}>
-          Pour ce voyage, il y a un risque de CHAKIB :
-        </Text>
-      </View>
-      <View style={{ backgroundColor: "yellow" }}>
-        <View style={styles.btn}>
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate("Mosquitoes");
-            }}
-          >
-            <View>
-              <Text>Moustiques</Text>
-              <Image
-                style={styles.icone}
-                source={require("../assets/general/icone-quotidien/moustique.png")}
-              ></Image>
-            </View>
-          </TouchableOpacity>
-        </View>
-      </View>
+      <BtnIcone
+        title={"Moustiques"}
+        iconeBtn={require("../assets/general/icone-quotidien/moustique.png")}
+        page="Mosquitoes"
+      ></BtnIcone>
+      <BtnIcone
+        title={"Tiques"}
+        iconeBtn={require("../assets/quotidien/animaux/tique.png")}
+        page="Ticks"
+      ></BtnIcone>
     </View>
   );
 }
@@ -39,24 +28,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center"
-  },
-  btn: {
-    height: 58,
-    alignItems: "center",
-    justifyContent: "center",
-    marginHorizontal: 25,
-    borderColor: "#3794B5",
-    borderRadius: 50,
-    borderWidth: 1,
-    borderStyle: "solid"
-  },
-  txt: {
-    fontSize: 20,
-    textAlign: "center",
-    color: "#3794B5"
-  },
-  icone: {
-    width: 58,
-    height: 58
   }
 });
--- screens/MyHealthProfile.js
+++ screens/MyHealthProfile.js
@@ -2,126 +2,47 @@ import React from "react";
 import { useNavigation } from "@react-navigation/core";
 import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 import HeaderTopImage from "../components/HeaderTopImage";
+import BtnIcone from "../components/BtnIcone";
 
 export default function MyHealthProfile() {
   const navigation = useNavigation();
   return (
-    <View style={styles.container}>
+    <View>
       <HeaderTopImage title={"Mon profil santé"}></HeaderTopImage>
-      <View
-        style={{
-          flex: 1,
-          width: "100%",
-          height: "100%"
-        }}
-      >
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate("MyHealth");
-          }}
-          style={styles.btn}
-        >
-          <Image
-            style={styles.icone}
-            source={require("../assets/general/icone-sante/etat_de_sante.png")}
-          ></Image>
-          <Text style={styles.onglet}>Ma santé</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate("MyVaccinationCard");
-          }}
-          style={styles.btn}
-        >
-          <Image
-            style={styles.icone}
-            source={require("../assets/general/icone-sante/vaccin.png")}
-          ></Image>
-          <Text style={styles.onglet}>Carnet de vaccination</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate("MyHealthProfile");
-          }}
-          style={styles.btn}
-        >
-          <Image
-            style={styles.icone}
-            source={require("../assets/general/icone-sante/medicament.png")}
-          ></Image>
-          <Text style={styles.onglet}>Médicaments</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate("MyHealthProfile");
-          }}
-          style={styles.btn}
-        >
-          <Image
-            style={styles.icone}
-            source={require("../assets/general/icone-sante/trousse_de_secours.png")}
-          ></Image>
-          <Text style={styles.onglet}>Trousse de secours</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate("MyHealthProfile");
-          }}
-          style={styles.btn}
-        >
-          <Image
-            style={styles.icone}
-            source={require("../assets/general/icone-sante/consultation.png")}
-          ></Image>
-          <Text style={styles.onglet}>Consultation médicale</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate("MyHealthProfile");
-          }}
-          style={styles.btn}
-        >
-          <Image
-            style={styles.icone}
-            source={require("../assets/general/icone-sante/couverture_sante.png")}
-          ></Image>
-
-          <Text style={styles.onglet}>Couverture santé</Text>
-        </TouchableOpacity>
-      </View>
+      <BtnIcone
+        title={"Ma santé"}
+        iconeBtn={require("../assets/general/icone-sante/etat_de_sante.png")}
+        page="MyHealth"
+      ></BtnIcone>
+      <BtnIcone
+        title={"Carnet de vaccination"}
+        iconeBtn={require("../assets/general/icone-sante/vaccin.png")}
+        page="MyVaccinationCard"
+      ></BtnIcone>
+      <BtnIcone
+        title={"Médicaments"}
+        iconeBtn={require("../assets/general/icone-sante/medicament.png")}
+      ></BtnIcone>
+      <BtnIcone
+        title={"Trousse de secours"}
+        iconeBtn={require("../assets/general/icone-sante/trousse_de_secours.png")}
+      ></BtnIcone>
+      <BtnIcone
+        title={"Consultation médicale"}
+        iconeBtn={require("../assets/general/icone-sante/consultation.png")}
+      ></BtnIcone>
+      <BtnIcone
+        title={"Couverture santé"}
+        iconeBtn={require("../assets/general/icone-sante/couverture_sante.png")}
+      ></BtnIcone>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "white",
     flex: 1,
     alignItems: "center",
     justifyContent: "center"
-  },
-  onglet: {
-    fontSize: 20,
-    textAlign: "center",
-    color: "#3794B5"
-  },
-  btn: {
-    height: 58,
-    backgroundColor: "white",
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 40,
-    marginHorizontal: 25,
-    borderColor: "#3794B5",
-    borderRadius: 50,
-    borderWidth: 1,
-    borderStyle: "solid"
-  },
-  icone: {
-    width: 55,
-    height: 55,
-    marginTop: 23,
-    marginRight: 330
   }
 });
