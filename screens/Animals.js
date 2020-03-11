2983b558778d30e278becb1cf7237ae715fdf4d4
Chakib B <57419800+chakhub@users.noreply.github.com>
2020-03-11 18:46:41 +0100

    built and fetched data for animals screen

--- screens/Animals.js
+++ screens/Animals.js
@@ -1,27 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import {
   StyleSheet,
   Text,
   View,
   TouchableOpacity,
-  ScrollView
+  ScrollView,
+  ActivityIndicator
 } from "react-native";
 import HeaderTopImage from "../components/HeaderTopImage";
+import axios from "axios";
 
-export default function Animals({ userToken }) {
+export default function Animals({ userToken, route }) {
+  const [getInfo, setGetInfo] = useState();
+  const [showInfo1, setShowInfo1] = useState(false);
+  const [showInfo2, setShowInfo2] = useState(false);
+  const [showInfo3, setShowInfo3] = useState(false);
+  const [isLoading, setIsloading] = useState(true);
   const navigation = useNavigation();
+  const id = route.params.travelId;
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `https://c0d1syq1s6.execute-api.eu-central-1.amazonaws.com/dev/travel/${id}/advice/animals`,
+        {
+          headers: {
+            Authorization: "Bearer " + userToken
+          }
+        }
+      );
+      setGetInfo(response.data);
+      setIsloading(false);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <ScrollView style={styles.container}>
       <View>
-        <HeaderTopImage title={"Animaux"} page="Daily"></HeaderTopImage>
-        <View
-          style={{
-            flex: 1,
-            width: "100%",
-            height: "100%"
-          }}
-        ></View>
+        <HeaderTopImage title={"Animaux"} page="Daily" />
+        {isLoading === true ? (
+          <ActivityIndicator />
+        ) : (
+          <View>
+            <Text>{getInfo.introduction}</Text>
+            <Text>{getInfo.subsections}</Text>
+          </View>
+        )}
       </View>
     </ScrollView>
   );
