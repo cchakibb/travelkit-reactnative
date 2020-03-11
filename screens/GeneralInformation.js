0898a70b0dfe00c286f63e231186cff4110e0e23
Chakib B <57419800+chakhub@users.noreply.github.com>
2020-03-11 22:47:10 +0100

    built the general info screen

--- screens/GeneralInformation.js
+++ screens/GeneralInformation.js
@@ -1,13 +1,25 @@
-import React, { useState } from "react";
-import { StyleSheet, View, ScrollView } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  View,
+  ScrollView,
+  ActivityIndicator,
+  Text
+} from "react-native";
 import { useNavigation, useRoute } from "@react-navigation/core";
 import HeaderTopImage from "../components/HeaderTopImage";
 import BtnInfo from "../components/BtnInfo";
 import { HitTestResultTypes } from "expo/build/AR";
 import axios from "axios";
+import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
 
 export default function GeneralInformation({ userToken, route }) {
-  const [getMoreInfo, setGetMoreInfo] = useState();
+  const [getInfo, setGetInfo] = useState();
+  const [showInfo1, setShowInfo1] = useState(false);
+  const [showInfo2, setShowInfo2] = useState(false);
+  const [showInfo3, setShowInfo3] = useState(false);
+  const [showInfo4, setShowInfo4] = useState(false);
+  const [showInfo5, setShowInfo5] = useState(false);
   const [isLoading, setIsloading] = useState(true);
   const navigation = useNavigation();
   const { params } = useRoute();
@@ -16,23 +28,67 @@ export default function GeneralInformation({ userToken, route }) {
   // route.params.travelId
 
   const fetchData = async () => {
-    const response = await axios.get(
-      `https://c0d1syq1s6.execute-api.eu-central-1.amazonaws.com/dev/travel/${id}/advice/generalinfo`,
-      {
-        headers: {
-          Authorization: "Bearer " + userToken
+    try {
+      const response = await axios.get(
+        `https://c0d1syq1s6.execute-api.eu-central-1.amazonaws.com/dev/travel/${id}/advice/generalinfo`,
+        {
+          headers: {
+            Authorization: "Bearer " + userToken
+          }
         }
-      }
-    );
+      );
+      setGetInfo(response.data);
+      setIsloading(false);
+    } catch (error) {
+      alert(error.message);
+    }
   };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     // TODO remember isLoading ?
     <ScrollView style={styles.container}>
       <View>
-        <HeaderTopImage title={"Infos générales"} page="Daily"></HeaderTopImage>
-        <BtnInfo title={"Dengue"}></BtnInfo>
-        <BtnInfo title={"Paludisme"}></BtnInfo>
+        <HeaderTopImage title={"Infos générales"} page="Daily" />
+        {isLoading === true ? (
+          <ActivityIndicator />
+        ) : (
+          <View>
+            <FlatList
+              data={getInfo.destinationInfo}
+              keyExtractor={item => {
+                return String(item.index);
+              }}
+              renderItem={({ item }) => <Text>{item}</Text>}
+            />
+            <TouchableOpacity onPress={() => setShowInfo1(!showInfo1)}>
+              <BtnInfo title={getInfo.sections[0].title} />
+            </TouchableOpacity>
+            {showInfo1 ? <Text>{getInfo.sections[0].subsections}</Text> : null}
+            <TouchableOpacity onPress={() => setShowInfo2(!showInfo2)}>
+              <BtnInfo title={getInfo.sections[1].title} />
+            </TouchableOpacity>
+            {showInfo2 ? <Text>{getInfo.sections[1].subsections}</Text> : null}
+            <TouchableOpacity onPress={() => setShowInfo3(!showInfo3)}>
+              <BtnInfo title={getInfo.sections[2].title} />
+            </TouchableOpacity>
+            {showInfo3 ? <Text>{getInfo.sections[2].subsections}</Text> : null}
+            <TouchableOpacity onPress={() => setShowInfo4(!showInfo4)}>
+              <BtnInfo title={getInfo.sections[3].title} />
+            </TouchableOpacity>
+            {showInfo4 ? <Text>{getInfo.sections[3].subsections}</Text> : null}
+            <TouchableOpacity onPress={() => setShowInfo4(!showInfo4)}>
+              <BtnInfo title={getInfo.sections[3].title} />
+            </TouchableOpacity>
+            {showInfo4 ? <Text>{getInfo.sections[3].subsections}</Text> : null}
+            <TouchableOpacity onPress={() => setShowInfo5(!showInfo5)}>
+              <BtnInfo title={getInfo.sections[4].title} />
+            </TouchableOpacity>
+            {showInfo5 ? <Text>{getInfo.sections[4].subsections}</Text> : null}
+          </View>
+        )}
       </View>
     </ScrollView>
   );
